953d8bebcf3170462c1beb8a2db26409c46f74b3
9719skumar <117668985+9719skumar@users.noreply.github.com>
2026-06-21 14:51:24 +0000

    conversion of datatypes

---
+++ 03_conversion_operation.js
@@ -0,0 +1,25 @@
+// let score = "33";
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+let score = "Sandeep";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+var valueInNumber = Number(score);
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+
+// let isloggedin = 1;
+// let isloggedin = 'sandeep kumar';
+let isloggedin = '';
+let BooleanisLoggedin = Boolean(isloggedin);
+// console.log(BooleanisLoggedin);
+
+//  1 =>true , 0 => false
+//  "" => false
+//  "Sandeep" => true
+
+let someNumber = 100;
+let convertInString = String(someNumber);
+console.log(typeof convertInString);
