64eca66f6f4866912008804ef02438b9936b97f7
9719skumar <117668985+9719skumar@users.noreply.github.com>
2026-06-21 14:20:14 +0000

    datatypes commit

---
+++ 02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treat all js code as new version
+
+// alert('Hello world')  we are using node js not browser
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof 'sandeepkumar');
+console.log(typeof 1234);
+console.log(typeof 10.10);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
