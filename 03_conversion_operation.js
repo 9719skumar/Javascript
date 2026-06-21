8ee5ca5eb20843c04f10d51577144a1f4d707684
9719skumar <117668985+9719skumar@users.noreply.github.com>
2026-06-21 15:15:04 +0000

    conversion commit

--- 03_conversion_operation.js
+++ 03_conversion_operation.js
@@ -22,4 +22,29 @@ let BooleanisLoggedin = Boolean(isloggedin);
 
 let someNumber = 100;
 let convertInString = String(someNumber);
-console.log(typeof convertInString);
+// console.log(typeof convertInString);
+
+/*################################# OPERATIONS ##############################*/
+
+let value = 3;
+let negval = -value;
+// console.log(negval);
+
+let hello = 'hello';
+let name = 'sandeep kumar';
+let combine = hello + ' ' + name;
+// console.log(combine);
+
+// console.log('1' + 10);
+// console.log(1 + '10');
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + '2');
+
+// console.log(+true); => return 1 
+//  console.log(true+); => return error
+// console.log(+"");  => return 0'
+
+let gamecounter = 100;
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter);
