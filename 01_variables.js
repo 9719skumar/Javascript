06da3e471d088aefe65ece9d1a51e83fff2971cf
9719skumar <117668985+9719skumar@users.noreply.github.com>
2026-06-21 13:19:54 +0000

    variable declare

---
+++ 01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345;
+let accountEmail = "9719sandeepkumar@gmail.com";
+var accountpassword = "12345";
+accountCity = "Saharanpur";
+let accountstate;
+
+accountEmail = '9719skumar@gmail.com';
+accountpassword = 102030;
+accountCity = "noida";
+
+// console.log(accountId);
+// console.log(accountEmail)
+
+/*
+
+preferred not use var 
+because issue create functional and block scope
+
+*/
+console.table([accountId , accountEmail,accountpassword,accountCity,accountstate]);
